28d2eb799166a45fe112ee769e63f15c505c36d9
祁昊 <286383640@qq.com>
2024-05-06 21:26:11 +0800

    图片替换功能

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <script setup>
-import PopUpPage from "/src/components/test/pop-up-page.vue";
 import upload from "@/components/test/ceshi.vue";
+import emitter from "@/utils/emitter";
+import { ref } from "vue";
+
+let uurl = ref(" ")
+emitter.on('myurl',(value) => {
+  // console.log(value.url)
+  uurl.value = value.url;
+  console.log(uurl)
+})
+
+
 </script>
 
 <template>
-  <!-- <PopUpPage /> -->
   <upload />
+  <upload />
+  <upload />
+  <img :src="uurl" alt="图片1" />
+  <img :src="uurl" alt="图片2" />
+  <img :src="uurl" alt="图片3" />
 </template>
--- src/components/test/ceshi.vue
+++ src/components/test/ceshi.vue
@@ -1,39 +1,22 @@
-
 <template>
   <a-space direction="vertical" :style="{ width: '100%' }">
-    <a-upload
-      action="/"
-      :fileList="file ? [file] : []"
-      :show-file-list="false"
-      @change="onChange"
-      @progress="onProgress"
-    >
+    <a-upload action="/" :fileList="file ? [file] : []" :show-file-list="false" @change="onChange"
+      @progress="onProgress">
       <template #upload-button>
-        <div
-          :class="`arco-upload-list-item${
-            file && file.status === 'error' ? ' arco-upload-list-item-error' : ''
-          }`"
-        >
-          <div
-            class="arco-upload-list-picture custom-upload-avatar"
-            v-if="file && file.url"
-          >
+        <div :class="`arco-upload-list-item${file && file.status === 'error' ? ' arco-upload-list-item-error' : ''
+          }`">
+          <div class="arco-upload-list-picture custom-upload-avatar" v-if="file && file.url">
             <img :src="file.url" />
             <div class="arco-upload-list-picture-mask">
               <IconEdit />
             </div>
-            <a-progress
-              v-if="file.status === 'uploading' && file.percent < 100"
-              :percent="file.percent"
-              type="circle"
-              size="mini"
-              :style="{
+            <a-progress v-if="file.status === 'uploading' && file.percent < 100" :percent="file.percent" type="circle"
+              size="mini" :style="{
                 position: 'absolute',
                 left: '50%',
                 top: '50%',
                 transform: 'translateX(-50%) translateY(-50%)',
-              }"
-            />
+              }" />
           </div>
           <div class="arco-upload-picture-card" v-else>
             <div class="arco-upload-picture-card-text">
@@ -47,29 +30,23 @@
   </a-space>
 </template>
 
-<script>
+<script setup>
 import { IconEdit, IconPlus } from '@arco-design/web-vue/es/icon';
 import { ref } from 'vue';
+import emitter from '@/utils/emitter.ts';
 
-export default {
-  components: {IconPlus, IconEdit},
-  setup() {
-    const file = ref();
 
-    const onChange = (_, currentFile) => {
-      file.value = {
-        ...currentFile,
-        // url: URL.createObjectURL(currentFile.file),
-      };
-    };
-    const onProgress = (currentFile) => {
-      file.value = currentFile;
-    };
-    return {
-      file,
-      onChange,
-      onProgress
-    }
-  },
+const file = ref();
+
+const onChange = (_, currentFile) => {
+  file.value = {
+    ...currentFile,
+    // url: URL.createObjectURL(currentFile.file),
+  };
+  emitter.emit('myurl',file.value)
 };
+const onProgress = (currentFile) => {
+  file.value = currentFile;
+};
+
 </script>
