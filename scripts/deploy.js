ff29583f803dc967bd94128bd7f34ca81de85495
祁昊 <286383640@qq.com>
2024-11-11 09:49:02 +0800

    修改自动部署脚本

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,22 +1,24 @@
 // 这是自动部署脚本，执行npm run deploy，可以自动构建并部署到服务器
-import { exec } from 'child_process';
-import { promisify } from 'util';
+import { exec } from "child_process";
+import { promisify } from "util";
 
 const execAsync = promisify(exec);
 
 const deploy = async () => {
   try {
     // 构建项目
-    console.log('开始构建...');
-    await execAsync('npm run build');
-    
+    console.log("开始构建...");
+    await execAsync("npm run build");
+
     // rsync 同步到服务器
-    console.log('开始部署...');
-    await execAsync('rsync -avz dist/* qihao@10.130.35.137:/var/www/uipreview');
-    
-    console.log('部署完成!');
+    console.log("开始部署...");
+    await execAsync(
+      "rsync -avz --chmod=u=rwx,g=rwx,o=rx dist/* qihao@10.130.35.137:/var/www/uipreview"
+    );
+
+    console.log("部署完成!");
   } catch (error) {
-    console.error('部署失败:', error);
+    console.error("部署失败:", error);
     process.exit(1);
   }
 };
