146659c21268c375a9e1446d5e6f697af85bed26
祁昊 <286383640@qq.com>
2025-06-26 15:50:39 +0800

    更新图标路径，升级依赖包，修复webpack配置格式

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
-module: {
-  rules: [
-    // 其他规则...
-    {
-      test: /\.(png|jpe?g|gif|svg|webp)$/i,
-      type: "asset/resource",
-      generator: {
-        filename: "images/[name].[hash][ext]",
+module.exports = {
+  module: {
+    rules: [
+      // 其他规则...
+      {
+        test: /\.(png|jpe?g|gif|svg|webp)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "images/[name].[hash][ext]",
+        },
       },
-    },
-  ];
-}
+    ],
+  },
+};
